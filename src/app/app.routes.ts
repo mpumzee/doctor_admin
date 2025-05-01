2d61899a1676d31b82ae282f579bbc2108843733
Mpumzee <mpumzcode@gmail.com>
2025-05-01 08:00:22 +0200

    login works

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { UserRegistrationComponent } from './user-registration/user-registration.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'register', component: UserRegistrationComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent }
+
+];
